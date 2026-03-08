f9a42c4612489cc5df3f34e0c6ae144ce9fc51b6
Jesús Domínguez <jesusdominguez2004@gmail.com>
2026-03-08 15:57:22 -0500

    feat(playground): add type declaration files example - 002-type-declaration-files.d.ts: define HeroType and HeroInterface - 002-type-declaration-files.ts: import and use external type/interface - demonstrate object creation, property access, method calls, and comparisons

---
+++ playground/002-type-declaration-files.d.ts
@@ -0,0 +1,16 @@
+// Hello, TypeScript! @jdomingu19
+// Playground: TypeScript Declaration Diles (.d.ts)
+
+// 1. Define an external type
+export type HeroType = {
+  name: string;
+  age: number;
+  greet: () => void;
+};
+
+// 2. Define an external interface
+export interface HeroInterface {
+  name: string;
+  age: number;
+  greet: () => void;
+}
